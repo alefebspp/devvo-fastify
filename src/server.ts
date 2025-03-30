1043308a429bfac8e061741a9216a82bfc4e81c2
Alefe Bispo <alefe.bispo@onesight.com.br>
2025-03-30 11:01:06 -0300

    add host

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import app from "./app";
 const start = async () => {
   try {
     await AppDataSource.initialize();
-    await app.listen({ port: 3000 });
+    await app.listen({ port: 3000, host: "0.0.0.0" });
     console.log("Server running on http://localhost:3000");
   } catch (err) {
     console.error(err);
